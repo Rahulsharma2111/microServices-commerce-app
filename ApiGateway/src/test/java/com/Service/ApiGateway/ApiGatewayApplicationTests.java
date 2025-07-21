750e96fdff1e74dc11506fe26aad491a0823d7f8
Rahulsharma2111 <rstv3644@gmail.com>
2025-07-21 19:06:52 +0530

    create product apis

---
+++ ApiGateway/src/test/java/com/Service/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Service.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
